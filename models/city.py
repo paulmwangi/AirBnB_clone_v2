aa6b416a83d39415af7a1e48899bb41a2416ff64
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2024-05-25 19:29:05 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy import String, DateTime, Column, ForeignKey
+"""Defines the City class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """This is the class for City
+    """Represents a city for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table cities.
+
     Attributes:
-        state_id: The state id
-        name: input name
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
     """
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', backref='cities',
-                          cascade='all, delete-orphan')
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
